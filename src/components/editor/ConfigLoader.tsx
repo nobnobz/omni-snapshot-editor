be2c91a76eae20b5236d9efd46467269c2f6086b
Marvin <me@nadler.xyz>
2026-03-15 11:56:54 +0100

    style: increase UME Templates dropdown width on desktop

--- src/components/editor/ConfigLoader.tsx
+++ src/components/editor/ConfigLoader.tsx
@@ -563,7 +563,7 @@ export function ConfigLoader() {
                                             <DropdownMenuContent
                                                 align="center"
                                                 sideOffset={8}
-                                                className={cn(editorSurface.overlay, "w-[min(22rem,calc(100vw-1.125rem))] rounded-[1.4rem] p-1.5")}
+                                                className={cn(editorSurface.overlay, "w-[min(22rem,calc(100vw-1.125rem))] sm:w-[28rem] rounded-[1.4rem] p-1.5")}
                                             >
                                                 <DropdownMenuLabel className={cn(editorLoader.subtleMeta, "px-3 pt-1 pb-1 text-foreground/38")}>UME Templates</DropdownMenuLabel>
                                                 {templateDownloads.length === 0 ? (
